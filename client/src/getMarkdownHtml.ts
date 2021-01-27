d0f3fab9fa169a9078712f3655e07e4f4dc028fe
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-01-27 11:50:06 -0500

    Make links open in new tab

--- client/src/getMarkdownHtml.ts
+++ client/src/getMarkdownHtml.ts
@@ -3,6 +3,20 @@ import DOMPurify from 'dompurify'
 
 /** Convert markdown to HTML */
 export function getMarkdownHtml(markdownStr: string) {
-  const html = marked(markdownStr)
+  let html = marked(markdownStr)
+
+  DOMPurify.addHook('afterSanitizeAttributes', function (a) {
+    if (
+      a instanceof HTMLAnchorElement &&
+      'target' in a &&
+      a.href.startsWith('https://')
+    ) {
+      a.target = '_blank'
+      a.rel = 'noopener noreferrer'
+      a.title = '(opens in new tab)'
+      a.setAttribute('aria-description', '(opens in new tab)')
+    }
+  })
+
   return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } })
 }
