dbb9cf0c810d2729d2edd56d16df094a9a9f6ef2
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-01-27 12:10:20 -0500

    Only add DOMPurify hook once

--- client/src/getMarkdownHtml.ts
+++ client/src/getMarkdownHtml.ts
@@ -1,22 +1,22 @@
 import marked from 'marked'
 import DOMPurify from 'dompurify'
 
+DOMPurify.addHook('afterSanitizeAttributes', function (a) {
+  if (
+    a instanceof HTMLAnchorElement &&
+    'target' in a &&
+    a.href.startsWith('https://')
+  ) {
+    a.target = '_blank'
+    a.rel = 'noopener noreferrer'
+    a.title = '(opens in new tab)'
+    a.setAttribute('aria-description', '(opens in new tab)')
+  }
+})
+
 /** Convert markdown to HTML */
 export function getMarkdownHtml(markdownStr: string) {
   let html = marked(markdownStr)
-
-  DOMPurify.addHook('afterSanitizeAttributes', function (a) {
-    if (
-      a instanceof HTMLAnchorElement &&
-      'target' in a &&
-      a.href.startsWith('https://')
-    ) {
-      a.target = '_blank'
-      a.rel = 'noopener noreferrer'
-      a.title = '(opens in new tab)'
-      a.setAttribute('aria-description', '(opens in new tab)')
-    }
-  })
 
   return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } })
 }
